768853ca70bdd1cb1d36aa4bd0274beaa80f9c4e
PhABC <ph.castonguay@gmail.com>
2023-04-29 17:28:26 -0400

    Clean up github config type

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,20 +3,23 @@ import getEnvParameter from '../utils/config'
 
 dotenv.config()
 
+// Discord bot token
 export const DISCORD_BOT_TOKEN = getEnvParameter('DISCORD_BOT_TOKEN')
+
+// OpenAI GPT-4 API key
 export const GPT_API_KEY = getEnvParameter('GPT_API_KEY')
 
-// Alfred configs
-const ALFRED_APP_ID = Number(getEnvParameter('ALFRED_APP_ID'))
-const ALFRED_GITHUB_INSTALLATION_ID = Number(getEnvParameter('ALFRED_GITHUB_INSTALLATION_ID'))
-const ALFRED_CLIENT_ID = getEnvParameter('ALFRED_CLIENT_ID')
-const ALFRED_CLIENT_SECRET = getEnvParameter('ALFRED_CLIENT_SECRET')
-const ALFRED_GITHUB_PK = getEnvParameter('ALFRED_GITHUB_PK')
+// Github configs
+const ALFRED_GIT_APP_ID = Number(getEnvParameter('ALFRED_APP_ID'))
+const ALFRED_GIT_CLIENT_ID = getEnvParameter('ALFRED_CLIENT_ID')
+const ALFRED_GIT_INSTALLATION_ID = Number(getEnvParameter('ALFRED_GITHUB_INSTALLATION_ID'))
+const ALFRED_GIT_CLIENT_SECRET = getEnvParameter('ALFRED_CLIENT_SECRET')
+const ALFRED_GIT_PK = getEnvParameter('ALFRED_GITHUB_PK')
 
-export const AlfredConfig = {
-  appId: ALFRED_APP_ID,
-  installationId: ALFRED_GITHUB_INSTALLATION_ID,
-  clientID: ALFRED_CLIENT_ID,
-  clientSecret: ALFRED_CLIENT_SECRET,
-  privateKey: ALFRED_GITHUB_PK,
+export const AlfredGithubConfig = {
+  appId: ALFRED_GIT_APP_ID,
+  installationId: ALFRED_GIT_INSTALLATION_ID,
+  clientID: ALFRED_GIT_CLIENT_ID,
+  clientSecret: ALFRED_GIT_CLIENT_SECRET,
+  privateKey: ALFRED_GIT_PK,
 }
--- src/types/AlfredGithubConfig.ts
+++ src/types/AlfredGithubConfig.ts
@@ -1,4 +1,4 @@
-export interface AlfredConfig {
+export interface AlfredGithubConfig {
   appId: number,
   installationId: number,
   clientID: string,
